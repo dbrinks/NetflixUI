9072933ae2df74cb61e4961948d0b4869e7a4f25
Devon Brinks <brinks.d@gmail.com>
2015-10-26 19:01:24 -0700

    Using grunt-serve instead of node server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,70 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          console: true,
-          document: true,
-          window: true,
-          define: true,
-          require: true
-        }
-      }
-    },
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: "src/js",
-          dir: "public/js",
-          removeCombined: true,
-          optimize: "none",
-          modules: [
-            {
-              name: "main",
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    console: true,
+                    document: true,
+                    window: true,
+                    define: true,
+                    require: true
+                }
             }
-          ],
-          useStrict: true
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    noBuildTxt: true,
+                    baseUrl: "src/js",
+                    dir: "public/js",
+                    removeCombined: true,
+                    optimize: "none",
+                    modules: [{
+                        name: "main",
+                    }],
+                    useStrict: true
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'public/css/main.css': 'src/scss/main.scss'
+                }
+            },
+        },
+        serve: {
+            options: {
+                port: 8888,
+                serve: {
+                    path: 'public'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '*.scss',
+                tasks: ['sass']
+            },
+            scripts: {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint', 'requirejs']
+            }
         }
-      }
-    },
-    sass: {
-      dist: {
-        options: {
-	  style: 'expanded'
-	},
-	files: {
-	  'public/css/main.css': 'src/scss/main.scss'
-        }
-      },
-    },
-    watch: {
-      css: {
-        files: '*.scss',
-        tasks: ['sass']
-      },
-      scripts: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint', 'requirejs']
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-serve');
 
-  grunt.registerTask('default', ['sass', 'jshint', 'requirejs', 'watch']);
+    grunt.registerTask('default', ['sass', 'jshint', 'requirejs', 'serve', 'watch']);
 
 };
